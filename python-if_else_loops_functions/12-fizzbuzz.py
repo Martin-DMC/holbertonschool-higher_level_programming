f75a737ad443f2b0a50729e296a491d0bfc0eb60
Martin-DMC <the.chiperfil@gmail.com>
2025-05-07 17:28:08 +0000

    scripts python 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    mul_3 = "Fizz"
-    mul_5 = "Buzz"
     for i in range(1, 101):
-        if i % 5 == 0 and i % 3 == 0:
-            print(f"{mul_3}{mul_5}", end=" ")
-        elif i % 5 == 0:
-            if i == 100:
-                print(f"{mul_5}", sep=" ", end="\n")
-            print(f"{mul_5}", end=" ")
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
         elif i % 3 == 0:
-            print(f"{mul_3}", end=" ")
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
         else:
-            print(f"{i}", end=" ")
+            print(i, end=" ")
+    print("")
