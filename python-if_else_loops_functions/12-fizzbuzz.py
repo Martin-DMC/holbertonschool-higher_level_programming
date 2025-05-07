6657437bcf3025d20906271b942691e7b8e5b78c
Martin-DMC <the.chiperfil@gmail.com>
2025-05-07 17:07:04 +0000

    scripts python 8, 9, 10, 11, 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    mul_3 = "Fizz"
+    mul_5 = "Buzz"
+    for i in range(1, 101):
+        if i % 5 == 0 and i % 3 == 0:
+            print(f"{mul_3}{mul_5}", end=" ")
+        elif i % 5 == 0:
+            if i == 100:
+                print(f"{mul_5}")
+            print(f"{mul_5}", end=" ")
+        elif i % 3 == 0:
+            print(f"{mul_3}", end=" ")
+        else:
+            print(f"{i}", end=" ")
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    resultado = ""
+    for letra in str:
+        if ord('a') <= ord(letra) <= ord('z'):
+            mayuscula_ascii = ord(letra) - (ord('a') - ord('A'))
+            resultado += chr(mayuscula_ascii)
+        else:
+            resultado += letra
+    print(resultado)
