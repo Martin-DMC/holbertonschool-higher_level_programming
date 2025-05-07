a12e514e08196f30a8af760bfd15e9ee4bf08097
Martin-DMC <the.chiperfil@gmail.com>
2025-05-07 17:59:56 +0000

    primeros ejercicios de modulos e import

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    res = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, res))
+    res = sub(a, b)
+    print("{:d} - {:d} = {:d}".format(a, b, res))
+    res = mul(a, b)
+    print("{:d} * {:d} = {:d}".format(a, b, res))
+    res = div(a, b)
+    print("{:d} / {:d} = {:d}".format(a, b, res))
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+keyword import y modulos
