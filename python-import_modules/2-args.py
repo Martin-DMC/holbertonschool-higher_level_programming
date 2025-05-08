23fba335a4020f7bcf21e8d1cda00771f823386b
Martin-DMC <the.chiperfil@gmail.com>
2025-05-08 16:18:08 +0000

    argument. arguments:

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,12 +7,14 @@ if __name__ == "__main__":
 
     if argc == 0:
         print(f"{argc} argument.")
-    else:
-        print(f"{argc} arguments:")
-        i = 1
-        for arg in argv:
-            if arg[0] == '.':
-                continue
-            else:
+    elif argc > 0:
+        if argc == 1:
+            print(f"{argc} argument.")
+            print(f"1: {argv[1]}")
+        else:
+            i = 1
+            print(f"{argc} arguments:")
+            for arg in range(1, len(argv)):
+                arg = argv[i]
                 print(f"{i}: {arg}")
                 i += 1
