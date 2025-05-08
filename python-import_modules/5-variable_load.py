6efcd7558935aaba963917155d095af96e2c0140
Martin-DMC <the.chiperfil@gmail.com>
2025-05-08 18:30:12 +0000

    variable load

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import sys
+from variable_load_5 import a
 
 if __name__ == "__main__":
-    variable_load_5_modulo = sys.modules.get('variable_load_5')
-    if variable_load_5_modulo is not None:
-        variable = variable_load_5_modulo.a
-        print(variable)
+    variable = a
+    print(variable)
