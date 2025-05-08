1cbe9d6cd90412887ea0b9684777ce5465010fb7
Martin-DMC <the.chiperfil@gmail.com>
2025-05-08 19:19:23 +0000

    alfabeto alrevez

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     for i in range(122, 96, -1):
-        print("{:c}".format(i) if i % 2 == 0 else "{:c}".format(i - 32), end="")
+        print(
+            "{:c}".format(i) if i % 2 == 0 else "{:c}".format(i - 32), end=""
+            )
