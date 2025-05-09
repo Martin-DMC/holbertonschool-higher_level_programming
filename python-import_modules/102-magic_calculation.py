ca6d19e9e3c4abe26856e9e9ef8d9d73aa393171
Martin-DMC <the.chiperfil@gmail.com>
2025-05-09 14:10:26 +0000

    probando con file 102

---
+++ python-import_modules/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub  # Importaciones desde el módulo
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
