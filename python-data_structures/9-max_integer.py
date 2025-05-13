871901d3f83c691323193f58fb85c55198925671
Martin-DMC <the.chiperfil@gmail.com>
2025-05-13 13:29:00 +0000

    error en num negativo y listas vacias

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maximus = 0
-    for num in my_list:
-        if num > maximus:
-            maximus = num
-    return maximus
+    if len(my_list) == 0:
+        return None
+    else:
+        maximus = my_list[0]
+        for num in my_list:
+            if num > maximus:
+                maximus = num
+        return maximus
