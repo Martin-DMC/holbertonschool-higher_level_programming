540950fdb556d783cc727a703e304fb1045785d6
Martin-DMC <the.chiperfil@gmail.com>
2025-05-13 10:54:21 +0000

    errores en ejercicio 2

--- python-data_structures/2-main.py
+++ python-data_structures/2-main.py
@@ -9,3 +9,30 @@ new_list = replace_in_list(my_list, idx, new_element)
 print(new_list)
 print(my_list)
 
+my_list = [1, 2, 3]
+idx = 0
+new_element = 4
+new_list = replace_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3]
+idx = 3
+new_element = 4
+new_list = replace_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
+
+my_list = [1]
+idx = 0
+new_element = 4
+new_list = replace_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
+
+my_list = [1]
+idx = 1
+new_element = 4
+new_list = replace_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
