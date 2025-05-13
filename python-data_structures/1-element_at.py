a0084a1eaf4f96867c60af46119a5fd010c0546f
Martin-DMC <the.chiperfil@gmail.com>
2025-05-13 10:31:19 +0000

    errores en ejercicio 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    valor = int(my_list[0])
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         for i in my_list:
             if i == idx:
-                valor = my_list[i]
+                valor = int(my_list[i])
         return valor
