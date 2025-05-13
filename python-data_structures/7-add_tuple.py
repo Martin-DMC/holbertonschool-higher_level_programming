85949438cfe0e8b79f347d35cde572f7903ede12
Martin-DMC <the.chiperfil@gmail.com>
2025-05-13 12:40:16 +0000

    ejercicio 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def crear_tuple(tuple_=()):
+    if len(tuple_) == 0:
+        tuple_ = (0, 0)
+        return tuple_
+    elif len(tuple_) == 1:
+        tuple_ = (tuple_[0], 0)
+        return tuple_
+    elif len(tuple_) >= 2:
+        tuple_ = (tuple_[0], tuple_[1])
+        return tuple_
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = list(crear_tuple(tuple_a))
+    tuple_b = list(crear_tuple(tuple_b))
+    valor_1 = (tuple_a[0] + tuple_b[0])
+    valor_2 = (tuple_a[1] + tuple_b[1])
+    new_tuple = (valor_1, valor_2)
+    return new_tuple
