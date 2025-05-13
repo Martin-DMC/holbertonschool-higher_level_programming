a69fdcc33ecd0fe84b66681c638cb5dcc7eda487
Martin-DMC <the.chiperfil@gmail.com>
2025-05-13 13:21:38 +0000

    era sin max, (ni comentado)

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    res = max(my_list)
-    return res
+    maximus = 0
+    for num in my_list:
+        if num > maximus:
+            maximus = num
+    return maximus
