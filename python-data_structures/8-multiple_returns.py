cd8b2154890486c15a19d7ea39dc5c09e40844e4
Martin-DMC <the.chiperfil@gmail.com>
2025-05-13 13:00:36 +0000

    error ejercicio 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     largo = len(sentence)
-    first = sentence[0]
+    if largo == 0:
+        first = None
+    else:
+        first = sentence[0]
 
     return largo, first
