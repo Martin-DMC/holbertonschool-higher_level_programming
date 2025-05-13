5ce6bf20680cb9ba89f56016509a578b1d459407
Martin-DMC <the.chiperfil@gmail.com>
2025-05-13 13:20:35 +0000

    ejercicio 9 con max :/

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    #res = max(my_list)
-    maximus = 0
-    for num in my_list:
-        if num > maximus:
-            maximus = num
-    return maximus
+    res = max(my_list)
+    return res
