2adef62855d886ae055f337fa88cee2228df19e2
Martin-DMC <the.chiperfil@gmail.com>
2025-05-14 17:25:30 +0000

    ejercicio 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary == None:
+        first_element = next(iter(a_dictionary.items()))
+        key = first_element[0]
+        value = first_element[1]
+        for clave, valor in a_dictionary.items():
+            if int(valor) > value:
+                key = clave
+        return key
+    else:
+        return None
