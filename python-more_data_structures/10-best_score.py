ce854b2c8c626164643317a02a8797552467b9ef
Martin-DMC <the.chiperfil@gmail.com>
2025-05-14 18:05:39 +0000

    ejercicio 10 con arreglos

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
+        return None
+    else:
         first_element = next(iter(a_dictionary.items()))
         key = first_element[0]
         value = first_element[1]
@@ -8,5 +10,3 @@ def best_score(a_dictionary):
             if int(valor) > value:
                 key = clave
         return key
-    else:
-        return None
