a6f87f4f0f0bdab1f89cc8333133a8052ce71340
Martin-DMC <the.chiperfil@gmail.com>
2025-05-14 15:41:06 +0000

    ejercicio 4 sin errores

--- python-more_data_structures/4-main.py
+++ python-more_data_structures/4-main.py
@@ -5,3 +5,8 @@ set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
 od_set = only_diff_elements(set_1, set_2)
 print(sorted(list(od_set)))
+
+set1 = { "Python", "Javascript" }
+set2 = { "Python", "Javascript" }
+od_set = only_diff_elements(set1, set2)
+print(sorted(list(od_set)))
--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    s1 = list(set_1)
-    s2 = list(set_2)
-    for num1 in s1:
-        for num2 in s2:
-            if num1 == num2:
-                s1.remove(num1)
-                s2.remove(num2)
-
-    retorno = s1 + s2
-    retorno = set(retorno)
-
-    return retorno
+    return set_1 ^ set_2
