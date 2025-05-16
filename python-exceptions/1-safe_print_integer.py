11f670c764f0674e7f1b34618c9f69ca05c916a5
Martin-DMC <the.chiperfil@gmail.com>
2025-05-16 13:56:56 +0000

    ultima prueba loca

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if int(value):
-            print("{:d}".format())
-            return True
+        int(value)
+        print("{:d}".format())
 
     except ValueError:
         return False
+
+    return True
