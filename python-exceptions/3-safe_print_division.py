815d2fa14d9e51531e5093d1036420b433028da6
Martin-DMC <the.chiperfil@gmail.com>
2025-05-16 15:34:32 +0000

    ejercicio 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,13 +2,9 @@
 def safe_print_division(a, b):
     resultado = None
     try:
-        if a is None or b is None:
-            raise TypeError("Los argumentos no pueden ser None")
-        resultado = a / b
+        resultado = (a / b)
     except ZeroDivisionError:
-        return None
-    except TypeError as e:
-        resultado = None
+        None
     finally:
         if resultado is not None:
             print("Inside result: {:.2f}".format(resultado))
