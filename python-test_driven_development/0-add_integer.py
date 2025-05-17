8a1c8061e2788b3c26c88e8aa04e768c17d8263d
Martin-DMC <the.chiperfil@gmail.com>
2025-05-17 19:22:48 +0000

    ejercicio 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,8 +11,6 @@ def add_integer(a, b=98):
         a: 1er numero.
         b: 2do numero. por default 98.
     """
-    #!/usr/bin/python3
-def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
