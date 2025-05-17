a70437a690d836d597d0b7f4e0a343d6f8ffe441
Martin-DMC <the.chiperfil@gmail.com>
2025-05-17 03:44:23 +0000

    ejercicio 4, 2da supuesta solucion

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    if not isinstance(my_list_1, list) or my_list_1 is None:
-        raise TypeError("my_list_1 debe ser una lista y no puede ser None")
-    if not isinstance(my_list_2, list) or my_list_2 is None:
-        raise TypeError("my_list_2 debe ser una lista y no puede ser None")
-    if list_length < 0:
-        raise ValueError("list_length no puede ser menor que 0")
     i = 0
     num = 0
     new_list = []
     max_length = max(len(my_list_1), len(my_list_2))
     while i < list_length and i < max_length:
         try:
-            a = int(my_list_1[i])
-            b = int(my_list_2[i])
+            a = my_list_1[i]
+            b = my_list_2[i]
             num = a / b
             num = round(num, 1)
         except (TypeError, ValueError):
