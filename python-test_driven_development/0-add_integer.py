f13824c19c310cfb873e8bcacb55d672a4d00799
Martin-DMC <the.chiperfil@gmail.com>
2025-05-17 19:07:32 +0000

    ejercicio 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,4 +15,19 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+
+    try:
+        int_a = int(a)
+    except OverflowError:
+        raise TypeError("a cannot be converted to an integer (overflow)")
+    except ValueError:
+        raise TypeError("a must be an integer or float (not NaN)")
+
+    try:
+        int_b = int(b)
+    except OverflowError:
+        raise TypeError("b cannot be converted to an integer (overflow)")
+    except ValueError:
+        raise TypeError("b must be an integer or float (not NaN)")
+
+    return int_a + int_b
