de09c659c540129b50f2300cd01e9f9fa7a0f7e9
Martin-DMC <the.chiperfil@gmail.com>
2025-05-19 13:48:46 +0000

    creando metodos de instancia

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -23,6 +23,8 @@ class Square():
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+
     def area(self):
         """Calcula y retorna el área del cuadrado.
 
