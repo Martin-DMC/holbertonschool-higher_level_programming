45abfb52d24ff026f1d142f255c8e3d1ef48713a
Martin-DMC <the.chiperfil@gmail.com>
2025-05-19 12:58:12 +0000

    creando una instancia

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+descripcion del modulo
+crea una clase Square() vacia
+"""
+
+
+class Square():
+    """
+    aca va la documentacion de la clase Square
+    crea la clase Square() vacia
+    """
+    def __init__(self, size):
+        """documentacion de instancia
+        arg:
+            size = lado del cuadrado de esta instancia"""
+        self.__size = size
