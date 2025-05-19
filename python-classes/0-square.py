5e0ae83d26f5a9af1d1b071a0fcbb86f18b98005
Martin-DMC <the.chiperfil@gmail.com>
2025-05-19 12:27:24 +0000

    espero que este todo

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+descripcion del modulo
+crea una clase Square() vacia
+"""
+
+
 class Square():
     """
-    aca va la documentacion de la
-    clase Square
+    aca va la documentacion de la clase Square
+    crea la clase Square() vacia
     """
-
     pass
