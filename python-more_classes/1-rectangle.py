071a5d5f9151665686401ce31bcb3f1ff6ee9c9c
Martin-DMC <the.chiperfil@gmail.com>
2025-05-20 15:41:54 +0000

    clase rectangle con instancias get and set

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+crea la clase Rectangle que esta vacia
+"""
+
+
+class Rectangle():
+    """
+    documentacion para la futura clase Rectangle()
+    """
+    def __init__(self, width=0, height=0):
+        """
+        documentacion de la instancia
+        args:
+            width: ancho
+            height: largo
+        Raises:
+            width o height != int --> TypeError
+            width o height < 0 --> ValueError
+        """
+        if not isinstance(width, int) or not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        if width < 0 or height < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    # -- GETTER --
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    # -- SETTER --
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
