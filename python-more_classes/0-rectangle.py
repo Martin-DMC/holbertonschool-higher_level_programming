6105d7366283663c363a570d8dbc1e6f8d8fef9e
Martin-DMC <the.chiperfil@gmail.com>
2025-05-20 14:40:56 +0000

    creando la clase Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+crea la clase Rectangle que esta vacia
+"""
+class Rectangle():
+    """
+    documentacion para la futura clase Rectangle()
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+mas sobre clases en python
