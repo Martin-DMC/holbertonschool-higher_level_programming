cb006b6dedf541a3e254dfd894b10e229fc1575b
Martin-DMC <the.chiperfil@gmail.com>
2025-05-26 16:36:39 +0000

    primer ejercicio

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+modulo que define una funcion que devuelve una lista de lo
+que contiene el objeto a examinar
+"""
+
+
 def lookup(obj):
+    """
+    funcion que devuelve la lista dicha
+    """
     return dir(obj)
