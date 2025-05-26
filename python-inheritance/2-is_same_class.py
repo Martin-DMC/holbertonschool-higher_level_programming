57efac2b879d2d7c5c12d8bf44fa8599f5f67447
Martin-DMC <the.chiperfil@gmail.com>
2025-05-26 21:50:59 +0000

    ejercicio 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
 modulo que define una funcion para saber si eñ objeto
-es exactamente una intancia de la clase especificada 
+es exactamente una intancia de la clase especificada
 """
+
+
 def is_same_class(obj, a_class):
     """
     dicha funcion, devuelve True si es exactamente
     o False en cualquier otra instancia
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
