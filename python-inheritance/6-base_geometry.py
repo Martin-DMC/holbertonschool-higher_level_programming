5c857d35de0baf664cc5578bb00e3fe90e906788
Martin-DMC <the.chiperfil@gmail.com>
2025-05-27 14:13:56 +0000

    6to ejercicio

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+modulo que cre la clase BaseGeometry()
+"""
+
+
+class BaseGeometry():
+    """
+    clase BaseGeometry
+    en este caso esta vacia
+    """
+    def area(self):
+        """
+        funcion que va a calcular el area del objeto
+        """
+        raise Exception("area() is not implemented")
