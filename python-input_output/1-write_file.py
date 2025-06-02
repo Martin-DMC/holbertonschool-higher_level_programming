843dce5e086bb4d89292f9621cc8893b58648ad3
Martin-DMC <the.chiperfil@gmail.com>
2025-06-02 16:19:30 -0300

    Create 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+"""
+define la funcion write_file
+la cual escribe en un archivo o lo crea si no existe
+y retorna los caracteres escritos
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    la funcion abre el archivo de una manera
+    que si no existe lo crea.
+    return:
+        numero de caracteres escritos
+    """
+    with open(filename, "w") as archivo:
+        charters = archivo.write(text)
+        return charters
