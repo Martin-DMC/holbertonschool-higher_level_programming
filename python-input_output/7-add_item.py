58896648f3c649ffa5959d54af18ec9a0497bf9b
Martin-DMC <the.chiperfil@gmail.com>
2025-06-03 15:22:57 +0000

    7mo ejercicio

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+this module define a script,
+this function of the scrip is adds all arguments to a
+Python list, and then save them to a file.
+"""
+
+
+import json
+import sys
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_json = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    lista = load_json("add_item.json")
+    datos = sys.argv
+
+    for i, dato in enumerate(datos):
+        if i == 0:
+            pass
+        else:
+            lista.append(dato)
+    save_to_json(lista, "add_item.json")
+except ():
+    lista = []
+    datos = sys.argv
+
+    for i, dato in enumerate(datos):
+        if i == 0:
+            pass
+        else:
+            lista.append(dato)
+    save_to_json(lista, "add_item.json")
