4c681a05cc7e118ac50b112a72c475293e1d3751
Martin-DMC <the.chiperfil@gmail.com>
2025-06-03 14:30:46 +0000

    6to ejercicio with function documentation

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """
 this module define the function load_from_json()
-this function create an objects fron JSON
+this function create an objects fron JSON file
 """
 import json
 
 
 def load_from_json_file(filename):
     """
+    this function open the json file in read mode for
+    after go print this information of the file
+    Return:
+        the data contain of the file
     """
     with open(filename, "r") as archivo:
         datos = json.load(archivo)
