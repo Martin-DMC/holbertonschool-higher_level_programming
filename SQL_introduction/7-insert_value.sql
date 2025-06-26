db2a62a6611aee37ef6f4c848f3d9763d558e6d4
Martin-DMC <the.chiperfil@gmail>
2025-06-26 00:24:17 +0000

    some exercises

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- inserting of new rows in the table of my database
+INSERT INTO first_table(id, name) VALUES(89, 'Best Scholl');
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- create a table second_table and add multiples rows
+CREATE TABLE IF NOT EXISTS second_table(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256), score INT);
+INSERT INTO second_table(name, score) VALUES('John', 10);
+INSERT INTO second_table(name, score) VALUES('Alex', 3);
+INSERT INTO second_table(name, score) VALUES('Bob', 14);
+INSERT INTO second_table(name, score) VALUES('George', 8);
