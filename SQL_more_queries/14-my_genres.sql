d1c4e9c3df408f0a6844736e44ddce397b003605
Martin-DMC <the.chiperfil@gmail.com>
2025-06-29 03:41:38 +0000

    exercise 14, 15, 16

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tg.name AS name FROM tv_show_genres AS tsg JOIN tv_genres AS tg ON tsg.genre_id = tg.id WHERE tsg.show_id = 8;
+SELECT tg.name AS name FROM tv_show_genres AS tsg JOIN tv_genres AS tg ON tsg.genre_id = tg.id JOIN tv_shows AS ts ON tsg.show_id = ts.id WHERE ts.title = 'Dexter';
--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,2 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT ts.title AS title FROM tv_shows AS ts JOIN tv_show_genres AS tg ON ts.id = tg.show_id WHERE tg.genre_id = 5;
+SELECT ts.title AS title FROM tv_shows AS ts JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id JOIN tv_genres AS tg ON tsg.genre_id = tg.id WHERE tg.name = 'Comedy' ORDER BY ts.title ASC;
