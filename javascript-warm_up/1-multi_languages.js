e44b347dd98b0c2f2c89557d2b90fbeee55e3926
Martin-DMC <the.chiperfil@gmail.com>
2025-07-13 20:59:31 +0000

    ejercicio 2

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const palabras = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+const palabras = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 const len = palabras.length;
 
 for (let i = 0; i < len; i++) {
