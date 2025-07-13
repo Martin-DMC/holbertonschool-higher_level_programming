577c84ec65ddc978a222718a2e1ef19b9accffce
Martin-DMC <the.chiperfil@gmail.com>
2025-07-13 21:18:01 +0000

    ejercicio 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-
 if (argv.length <= 2) {
   console.log('No argument');
-}
-else if (argv.length == 3) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  const argumentosDelUsuario = argv.slice(2);
+  console.log(argumentosDelUsuario.join(' '));
 }
