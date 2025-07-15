2e47461dc9444dfe86ea7f4a83752c564f3afca5
Martin-DMC <the.chiperfil@gmail.com>
2025-07-15 11:05:19 -0300

    ejercicio 6

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const divCharacter = document.getElementById('character');
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      divCharacter.innerHTML = `${data.name}`;
+    })
+    .catch(error => {
+      console.error('Error: ', error);
+      divCharacter.innerHTML = 'error del pinchi servidor';
+    });
+});
